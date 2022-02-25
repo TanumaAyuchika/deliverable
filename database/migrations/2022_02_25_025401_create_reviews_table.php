f031566187a65da95b4a909f02a64e1b8dadbc46
TanumaAyuchika <tanuayu19111161@gmail.com>
2022-02-25 12:11:23 +0900

    [add]reviewsテーブル

---
+++ database/migrations/2022_02_25_025401_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',50);
+            $table->string('body',250);
+            $table->float('amount',8,2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}

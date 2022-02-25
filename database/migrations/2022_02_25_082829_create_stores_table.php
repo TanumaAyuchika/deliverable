1ef632425c428f39d76f05fb7518a49a0e04a172
TanumaAyuchika <tanuayu19111161@gmail.com>
2022-02-25 19:15:14 +0900

    [add]migration

---
+++ database/migrations/2022_02_25_082829_create_stores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',50);//店舗名
+            $table->string('address');//住所
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}

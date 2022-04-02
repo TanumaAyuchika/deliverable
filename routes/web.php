353584f755d410b51f8f5be7934ccc61bf696847
TanumaAyuchika <tanuayu19111161@gmail.com>
2022-04-02 17:32:42 +0900

    [add]ログイン認証機能

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/','StoreController@index');
-Route::get('/stores/create','StoreController@create');
-Route::get('/stores/{store}/reviews','StoreController@show');
-Route::get('/stores/{store}/reviews/create','ReviewController@create');
-Route::post('/stores','StoreController@store');
-Route::post('/stores/{store}/reviews', 'ReviewController@store');
-Route::delete('/reviews/{review}', 'ReviewController@delete');
-
-Auth::routes();
+Route::group(['middleware' => ['auth']], function(){
+   Route::get('/','StoreController@index');
+   Route::get('/stores/create','StoreController@create');
+   Route::get('/stores/{store}/reviews','StoreController@show');
+   Route::get('/stores/{store}/reviews/create','ReviewController@create');
+   Route::post('/stores','StoreController@store');
+   Route::post('/stores/{store}/reviews', 'ReviewController@store');
+   Route::delete('/reviews/{review}', 'ReviewController@delete');
+});
 
 Auth::routes();
 
